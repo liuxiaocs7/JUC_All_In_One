f1f9f026c176b88aa903b83696ecb80443ddf49c
liuxiao <liuxiao2103@qq.com>
2021-07-23 01:08:15 +0800

    countDownLatch计数

---
+++ JUC_2021/src/com/liuxiaocs/juc/countDownLatchDemo.java
@@ -0,0 +1,31 @@
+package com.liuxiaocs.juc;
+
+import java.util.concurrent.CountDownLatch;
+
+// 演示CountDownLatch
+public class countDownLatchDemo {
+
+    // 6个同学陆续离开教室之后，班长锁门
+    public static void main(String[] args) throws InterruptedException {
+
+        // 创建CountDownLatch对象，设置初始值为6
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+
+        // 6个同学陆续离开教室之后
+        for (int i = 0; i < 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " 号同学离开了教室");
+
+                // 每个同学离开之后
+                // 计数器 - 1
+                countDownLatch.countDown();
+            }, String.valueOf(i)).start();
+        }
+
+        // 等待
+        countDownLatch.await();
+
+        // 6个同学都离开教室之后班长才可以锁门
+        System.out.println(Thread.currentThread().getName() + "班长锁门走人了");
+    }
+}
