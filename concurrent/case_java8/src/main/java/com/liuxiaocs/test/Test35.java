71069f66c7b1353807f6858a671e9905c94c8c03
liuxiao <liuxiao2103@qq.com>
2022-04-24 23:20:28 +0800

    feat: 测试原子引用类型

---
+++ concurrent/case_java8/src/main/java/com/liuxiaocs/test/Test35.java
@@ -0,0 +1,77 @@
+package com.liuxiaocs.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 测试原子引用类型
+ *
+ * 0 cost: 47 ms
+ */
+@Slf4j(topic = "c.Test35")
+public class Test35 {
+    public static void main(String[] args) {
+        DecimalAccount.demo(new DecimalAccountCas(new BigDecimal("10000")));
+    }
+}
+
+class DecimalAccountCas implements DecimalAccount {
+
+    private AtomicReference<BigDecimal> balance;
+
+    public DecimalAccountCas(BigDecimal balance) {
+        this.balance = new AtomicReference<>(balance);
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return balance.get();
+    }
+
+    @Override
+    public void withdraw(BigDecimal amount) {
+        while (true) {
+            BigDecimal prev = balance.get();
+            BigDecimal next = prev.subtract(amount);
+            if (balance.compareAndSet(prev, next)) {
+                break;
+            }
+        }
+    }
+}
+
+interface DecimalAccount {
+    // 获取余额
+    BigDecimal getBalance();
+
+    // 取款
+    void withdraw(BigDecimal amount);
+
+    /**
+     * 方法内会启动1000个线程，每个线程做-10元的操作
+     * 如果初始余额为10000那么正确的结果应当是0
+     */
+    static void demo(DecimalAccount account) {
+        List<Thread> ts = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            ts.add(new Thread(() -> {
+                account.withdraw(BigDecimal.TEN);
+            }));
+        }
+        long start = System.nanoTime();
+        ts.forEach(Thread::start);
+        ts.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        long end = System.nanoTime();
+        System.out.println(account.getBalance() + " cost: " + (end - start) / 1000_000 + " ms");
+    }
+}
